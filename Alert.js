ebe09a50e911ba18cbd27679095944dfa0566e02
Sameer <176995632+SamPeterberg@users.noreply.github.com>
2025-08-13 19:30:59 +0500

    Alert excercises done

---
+++ Alert.js
@@ -0,0 +1,9 @@
+alert("Welcome To Our website")
+
+alert("ERROR! Please Enter a valid password")
+
+alert("Welcome to JS land \n Happy coding")
+
+alert("Welcome to js land")
+
+alert("Happy coding!")
