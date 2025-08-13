60ecee35d5b91022173ea9dfa9e15287f9144d66
Sameer <176995632+SamPeterberg@users.noreply.github.com>
2025-08-13 19:41:28 +0500

    learned Variables For String with user prompts and alerts

---
+++ VariablesForString.js
@@ -0,0 +1,28 @@
+var userName="";
+
+var myName = "Sameer Ahmed"
+
+var message = "Hello World"
+alert(message)
+
+var name = prompt("Write your name")
+var age = prompt("Write your age")
+var course = prompt("Write your course name")
+alert(name)
+alert(age)
+alert(course)
+
+var pizza  = "PIZZA \N PIZZ \N PIZ \N PI \N P"
+alert(pizza)
+
+var email = "sameerahmedgorgaij@gmail.com"
+alert("My email address is "+ email)
+
+var book = "A SMARTER WAY TO LEARN JAVASCRIPT"
+alert("I am trying to learn from the book "+book)
+
+var html = "Yah! I can write HTML content through Javascript"
+document.write(html)
+
+var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+alert(design)
