b2eaffa824c20a411ca8a2f9f38c614827f7d958
Sameer <176995632+SamPeterberg@users.noreply.github.com>
2025-08-15 23:14:09 +0500

    Add arithmetic operations and ticket cost calculation to MathExpressions.js

--- MathExpressions.js
+++ MathExpressions.js
@@ -11,5 +11,33 @@ document.writeln(`multiplication of ${num1} and ${num2} is ${num1*num2}`)
 document.writeln(`division of ${num1} and ${num2} is ${num1/num2}`)
 
 
+var a;
+document.writeln(`Value after decleration is ${a}`)
+a= 5
+document.writeln(`initial value ${a}`)
+a = a++
+document.writeln(`Value after increment is ${a}`)
+a=a+5
+document.writeln(`Value after addition is ${a}`)
+a= a--
+document.writeln(`Value after decrement is ${a}`)
+a=a%a
+document.writeln(`The remainder is ${a}`)
+
+var movieTicket = 600
+document.writeln(`Total cost of buying 5 tickets is ${movieTicket*5}PKR`)
 
 
+var num = 6
+document.writeln(`Table of ${num}
+    ${num} X 1 = ${num*1}
+    ${num} X  = ${num*2}
+    ${num} X 3 = ${num*3}
+    ${num} X 4 = ${num*4}
+    ${num} X 5 = ${num*5}
+    ${num} X 6 = ${num*6}
+    ${num} X 7 = ${num*7}
+    ${num} X 8 = ${num*8}
+    ${num} X 9 = ${num*9}
+    ${num} X 10 = ${num*10}
+    `)
